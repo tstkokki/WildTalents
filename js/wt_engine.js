f92d34668e71eec77840db80552b1bfe7513e889
tstkokki <60537602+tstkokki@users.noreply.github.com>
2022-12-10 16:41:57 +0200

    electrocuting head

--- js/wt_engine.js
+++ js/wt_engine.js
@@ -13,7 +13,6 @@ function FormDamageResult(formData) {
   
     let { shock, killing, width, height } = ExtractWidthAndShockAndKilling();
   
-    let dmgTypes = GetDamageTypes();
     let shockDmg = width;
     let killingDmg = width;
     let deadly;
@@ -21,12 +20,12 @@ function FormDamageResult(formData) {
 
     const person = {
         HitLocations: [
-            {Dmg: {Shock: 0, Killing:0}, Name: "Head", Locations: [0]},
-            {Dmg: {Shock: 0, Killing:0}, Name: "Torso", Locations: [7,8,9]},
-            {Dmg: {Shock: 0, Killing:0}, Name: "Right Arm", Locations: [5,6]},
-            {Dmg: {Shock: 0, Killing:0}, Name: "Left Arm", Locations: [3,4]},
-            {Dmg: {Shock: 0, Killing:0}, Name: "Right Leg", Locations: [2]},
-            {Dmg: {Shock: 0, Killing:0}, Name: "Left Leg", Locations: [1]}
+            {Dmg: {Shock: 0, Killing:0}, Name: "Head", Locations: [0]}, //0
+            {Dmg: {Shock: 0, Killing:0}, Name: "Torso", Locations: [7,8,9]}, //1
+            {Dmg: {Shock: 0, Killing:0}, Name: "Right Arm", Locations: [5,6]}, //2
+            {Dmg: {Shock: 0, Killing:0}, Name: "Left Arm", Locations: [3,4]}, //3
+            {Dmg: {Shock: 0, Killing:0}, Name: "Right Leg", Locations: [2]}, // 4
+            {Dmg: {Shock: 0, Killing:0}, Name: "Left Leg", Locations: [1]} //5
         ],
         ApplyDamage: function(location, type, amount){
             this.HitLocations.forEach(h => {
@@ -41,6 +40,16 @@ function FormDamageResult(formData) {
                     h.Dmg[type] += amount;
                 }
             })
+        },
+        ApplyShockByName: function(location, amount){
+            this.ApplyDamageByName(location, "Shock", amount)
+        },
+        ApplyKillingByName: function(location, amount){
+            this.ApplyDamageByName(location, "Killing", amount)
+        },
+        ApplyShockAndKillingByName(location, amount){
+            this.ApplyShockByName(location,amount);
+            this.ApplyKillingByName(location, amount);
         },
         Reset: function(){
             this.HitLocations.forEach(h => {
@@ -58,7 +67,6 @@ function FormDamageResult(formData) {
         }
     };
 
-    console.log(shock, killing, width)
     formData.forEach(element => {
         if(element.value != 0){
 
@@ -66,20 +74,31 @@ function FormDamageResult(formData) {
                 case "Area":
                     //-if Area: target and everyone within radius takes 2 Shock to every location. 
                     //Each char in the area rolls dice = Area rating. Each die = 1K to rolled location
-                    locations.forEach(loc => {
-                        person.ApplyDamageByName(loc, "Shock", 2)
+                    person.HitLocations.forEach(loc => {
+                        person.ApplyShockByName(loc.Name, 2)
                     })
                     break;
                 case "Burn":
-                    locations.forEach(loc => {
-                        if(loc != "Head")
-                        person.ApplyDamageByName(loc, "Shock", 1)
+                    person.HitLocations.forEach(loc => {
+                        if(loc.Name != "Head")
+                        person.ApplyShockByName(loc.Name, 1)
                     })
                     //if Burn: every hit location except head takes 1 point of Shocking
                     break;
                 case "Daze":
                     break;
                 case "Electrocuting":
+                    let limb = Math.floor(Math.random()*2)+1;
+                    if(locations[height] == "Head"){
+                        if(shock != -1){
+                            person.ApplyShockByName("Torso", shockDmg)
+                            person.ApplyDamage(limb, "Shock", shockDmg)
+                        }
+                        if(killing != -1){
+                            person.ApplyKillingByName("Torso", killingDmg)
+                            person.ApplyDamage(limb, "Killing", shockDmg)
+                        }
+                    }
                     
                     // if Electrocuting: If the attack damages the target—it must inflict at least one point of
                     // damage past the target’s defenses—that same damage instantly “travels” to adjacent hit
@@ -98,17 +117,12 @@ function FormDamageResult(formData) {
           let roll = $("#dmgInput").val().toUpperCase();
           let indexOfWidth = roll.indexOf("W");
           let width = parseInt(roll.substr(0, indexOfWidth));
-          let shock = IndexOfChar(roll, "S");
-          let killing = IndexOfChar(roll, "K");
+          let shock = IndexOfChar(roll, "S") != -1 ? "Shock" : -1;
+          let killing = IndexOfChar(roll, "K") != -1 ? "Killing": -1;
           let height = parseInt(roll.substr(roll.indexOf("H")-1,1));
           return { shock, killing, width, height };
     };
 
-    function GetDamageTypes() {
-        return (shock != -1 ? "Shock" : "") +
-            (shock != -1 && killing != -1 ? " and " : "") +
-            (killing != -1 ? "Killing" : "");
-    }
   
     function GetDamageLocation(height){
         
